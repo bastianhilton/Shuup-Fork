d7b9d365b4abbbfc0a9bca71dc5e81ad6adaaa21
bastianhilton <bastianh86@outlook.com>
2021-07-25 13:21:25 -0700

    initial setup

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -20,4 +20,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r"^sa/", include("shuup.admin.urls", namespace="shuup_admin")),
     url(r"^", include("shuup.front.urls", namespace="shuup")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)git 
